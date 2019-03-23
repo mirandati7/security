5bbb080c1d16b8c28670571437c7e5cfb7530c5e
Alex Miranda <alex@localhost.localdomain>
2019-03-23 18:15:17 -0300

    first commit

---
+++ src/main/java/br/com/ga/security/App.java
@@ -0,0 +1,10 @@
+package br.com.ga.security;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Bem Vindo ao Bitbucket" );
+        System.out.println( "Mestre Alex e Aluno Gladson" );
+    }
+}
